4a2501489b067ad8e02f9777202a9b08fe92f181
unknown <ifanefendi666@gmail.com>
2024-07-25 01:42:49 +0700

    first commit git

---
+++ db_restoran.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Jul 2024 pada 20.33
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_restoran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `masakan`
+--
+
+CREATE TABLE `masakan` (
+  `id_masakan` int(11) NOT NULL,
+  `nama_masakan` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `masakan`
+--
+
+INSERT INTO `masakan` (`id_masakan`, `nama_masakan`, `harga`, `status`) VALUES
+(7, 'NASI PUTIH', 5000, 'TERSEDIA'),
+(8, 'AYAM POP', 15000, 'TERSEDIA'),
+(9, 'TEH MANIS', 4000, 'TERSEDIA'),
+(10, 'RENDANG SAPI', 18000, 'HABIS'),
+(11, 'TUNJANG', 15000, 'TERSEDIA'),
+(12, 'KERUPUK KULIT SIRAM', 8000, 'TERSEDIA'),
+(13, 'TELOR DADAR', 8000, 'TERSEDIA');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `nama_pelanggan` varchar(30) NOT NULL,
+  `id_masakan` int(30) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_masakan` varchar(30) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `total_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_user`, `id_level`) VALUES
+(0, 'admin', 'admin123', 'Ifan Efendi', 1),
+(12, 'kasir', 'kasir123', 'Rika Anggareni', 2),
+(13, 'owner', 'owner123', 'Bapak Tedy', 3),
+(14, 'pelanggan', 'pelanggan123', 'Anisa', 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `masakan`
+--
+ALTER TABLE `masakan`
+  ADD PRIMARY KEY (`id_masakan`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_masakan` (`id_masakan`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `masakan`
+--
+ALTER TABLE `masakan`
+  MODIFY `id_masakan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_masakan`) REFERENCES `masakan` (`id_masakan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/aplikasi_restoran_ifan/koneksi.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi_restoran_ifan;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ifan
+ */
+
+public class koneksi {
+    private final String url = "jdbc:mysql://localhost/db_restoran";
+    private final String username_xampp ="root";
+    private final String password_xampp ="";
+    private Connection con;
+    
+    public void connect (){
+        try {
+            con = DriverManager.getConnection(url, username_xampp, password_xampp);
+            System.out.println("Koneksi berhasil");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+    
+}
